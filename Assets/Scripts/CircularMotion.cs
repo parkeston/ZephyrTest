225b8adf51b0f4bab56dc27334e73a74c9acef26
parkeston <pasha.shpilevskiy@gmail.com>
2020-03-12 22:00:39 +0300

    Initial commit

---
+++ Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CircularMotion : MonoBehaviour
+{
+    [SerializeField] private float angularSpeed;
+    [SerializeField] private float motionRadius;
+
+    [SerializeField] private Vector3 centerPoint;
+    
+    
+    private void Start()
+    {
+        transform.position = centerPoint + Vector3.back * motionRadius;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            transform.RotateAround(centerPoint,Vector3.up, Time.deltaTime*angularSpeed);
+    }
+}
